47a12c3cf98ecb623c3617464f2d444bcd5ba997
SANTHOSHVV <>
2021-05-31 04:44:59 +0000

    Initial commit

---
+++ polls/views.py
@@ -0,0 +1,27 @@
+from django.http import HttpResponse
+from .models import Question
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+      'latest_question_list': latest_question_list,
+    }    
+    return HttpResponse(template.render(context, request))
+
+  
+
+def detail(request,question_id):
+  response ="You are looking at questions %s."
+  return HttpResponse(response % question_id)
+
+
+def results(request,question_id):
+  response = "You are looking at results %s."
+  return HttpResponse(response % question_id)
+
+def vote(request,question_id):
+  response = "You are looking at voting %s."
+  return HttpResponse(response % question_id)
